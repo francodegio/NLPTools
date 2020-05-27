cbe5e9606ad0360d241de415507caa6d7a5b5d56
Franco De Giovannini <fdegiovannini@moorea.io>
2020-05-27 09:23:43 -0300

    bugfixed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
+VERSION = '0.1.0'
+
 with open('README.md', 'r') as file:
-    long_description = file.head()
+    long_description = file.read()
+
 setup(
     name='nlptools',
-    version='0.1.0',
+    version=VERSION,
     description='Suite of tools for training models and mining text.',
     long_description=long_description,
     long_description_conten_type='text/markdown',
