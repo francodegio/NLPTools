cbe0e0967ecb715426ea0022a276e95c568a8908
Franco De Giovannini <fdegiovannini@moorea.io>
2020-05-29 09:47:54 -0300

    created new module named 'training'

--- build/lib/nlptools/data_augmentation.py
+++ build/lib/nlptools/data_augmentation.py
@@ -6,10 +6,12 @@ Classes
 - TaggedDoc
     Intended to open tagged documents using the Text Tag Tool.
 """
+import os
 import warnings
 import random
+import datetime
+from typing import List
 import pandas as pd
-import numpy as np
 from spacy import displacy
 from num2words import num2words
 
@@ -38,7 +40,7 @@ class TaggedDoc:
     -------
     - TaggedDoc.render
     - TaggedDoc.index_augmentation
-
+    - TaggedDoc.save_render
 
     
     """
@@ -149,14 +151,52 @@ class TaggedDoc:
 
 def random_date_generator(
         start_year:int=1900, 
-        end_year:int=2050, 
         start_month:int=1, 
-        end_month:int=12, 
         start_day:int=1, 
+        end_year:int=2050,
+        end_month:int=12, 
         end_day:int=31, 
         mapper:dict=None, 
         seed:int=None
     ) -> datetime.date:
+    """ Creates a random date in the time span provided. Returns a datetieme.date object.
+
+    Parameters
+    ----------
+    start_year : int, optional
+        The initial year of the target time span, by default 1900.
+    start_month : int, optional
+        The initial month of the target time span, by default 1.
+    start_day : int, optional
+        The initial day of the target time span, by default 1.
+    end_year : int, optional
+        The final year of the target time span, by default 2050.
+    end_month : int, optional
+        The final month of the target time span, by default 12.
+    end_day : int, optional
+        The final day of the target time span, by default 31.
+    mapper : dict, optional
+        A dictionary-like object with same previous arguments as keys and integer as values, by default None.
+    seed : int, optional
+        The random seed if you're interested in replicating the results, by default None.
+
+    Returns
+    -------
+    datetime.date
+        A random datetime.date object contained in the time span provided.
+
+
+    Examples
+    -------
+    >>> from nlptools.data_augmentation import random_date_generator
+    >>> random_date_generator()
+    datetime.date(1901, 6, 1)
+    >>> random_date_generator(1991,2,1, 1991, 2, 2)
+    datetime.date(1991, 2, 1)
+    >>> random_date_generator(mapper={'start_year': 2000, 'start_month': 1, 'start_day': 1,
+    ...                               'end_year': 2025, 'end_month':12, 'end_day':31})
+    datetime.date(2010, 10, 9)
+    """
     if 0 in [start_day, start_month, start_year, end_month, end_year, end_day]:
         warnings.warn('An argument was specified with value 0, returning to default values.')
         mapper = {'start_year':1900, 
@@ -176,7 +216,9 @@ def random_date_generator(
     
     if seed:
         random.seed(seed)
-        
+    
+    if start_year == end_year and start_month == end_month and start_day == end_day:
+        end_day = start_day + 1
     start_date = datetime.date(start_year, start_month, start_day)
     end_date = datetime.date(end_year, end_month, end_day)
     time_between_dates = end_date - start_date
@@ -187,13 +229,66 @@ def random_date_generator(
     return random_date
 
 
-def date_formatter(date: datetime.date, formality: str = 'random', include_year: bool = True) -> str:
-    formality_list = ['basic', 'basic2', 'mixed', 'mixed2', 'regular', 'formal', 'veryformal', 'random']
+def date_formatter(
+        date: datetime.date, 
+        formality: str = 'random', 
+        include_year: bool = True,
+        seed:int=None
+    ) -> str:
+    """ Converts a datetime.date object into a formatted string.
+
+    Parameters
+    ----------
+    date : datetime.date
+        The date object intended to convert to string.
+    formality : str, optional
+        Different types of formatting, by default 'random'.
+    include_year : bool, optional
+        Option to keep or leave the year, by default True.
+
+    Returns
+    -------
+    str
+        A string containing the desired format
+
+    Raises
+    ------
+    KeyError
+        If the formality provided is not among the accepted, will raise KeyError.
     
+    Examples
+    ------
+    >>> from nlptools.data_augmentation import random_date_generator
+    >>> date = random_date_generator()
+    >>> from nlptools.data_augmentation import date_formatter
+    >>> print(date)
+    1945-12-27
+    >>> date_formatter(date, 'basic')
+    '27-12-1945'
+    >>> date_formatter(date, 'basic2')
+    '27/12/1945'
+    >>> date_formatter(date, 'mixed')
+    '27 de Diciembre de 1945'
+    >>> date_formatter(date, 'mixed2')
+    '27 de Diciembre de mil novecientos cuarenta y cinco'
+    >>> date_formatter(date, 'regular')
+    'veintisiete de Diciembre de mil novecientos cuarenta y cinco'
+    >>> date_formatter(date, 'formal')
+    'veintisiete del mes de Diciembre de mil novecientos cuarenta y cinco'
+    >>> date_formatter(date, 'veryformal')
+    'vigésimo séptimo día del mes de Diciembre del año mil novecientos cuarenta y cinco'
+    >>> date_formatter(date, 'random')
+    'veintisiete de Diciembre de mil novecientos cuarenta y cinco'
+    >>> date_formatter(date, 'random', include_year=False)
+    '27 de Diciembre'
+    """
+    formality_list = ['basic', 'basic2', 'mixed', 'mixed2', 'regular', 'formal', 'veryformal', 'random']
+    if seed:
+        random.seed(seed)
     if formality not in formality_list:
         raise KeyError(f'Keyword `{formality}` not found. Argument `formality` must be one of {formality_list}.')
     elif formality == 'random':
-        formality = formality_list[random.randint(0,5)]
+        formality = formality_list[random.randint(0,6)]
     
     month_mapper = {
         '01':'Enero',
@@ -212,9 +307,8 @@ def date_formatter(date: datetime.date, formality: str = 'random', include_year:
     day = f'{date.day}' if len(str(date.day)) > 1 else f'0{date.day}'
     month = f'{date.month}' if len(str(date.month)) > 1 else f'0{date.month}'
     
-    if formality not in ['basic', 'basic2']:
-        day_words = num2words(day, lang='es', to='cardinal') if formality != 'veryformal' else num2words(day, lang='es', to='ordinal')
-        month_words = month_mapper[month]
+    day_words = num2words(day, lang='es', to='cardinal') if formality != 'veryformal' else num2words(day, lang='es', to='ordinal')
+    month_words = month_mapper[month]
     
     if include_year:
         year = f'{date.year}'    
@@ -239,7 +333,36 @@ def date_formatter(date: datetime.date, formality: str = 'random', include_year:
     return result
 
 
-def random_name_generator(name_type:str, n:int):
+def random_name_generator(
+        n: int, 
+        name_type: str = 'any',
+        seed:int=None
+    ) -> list:
+    """ Creates a list of len(n) names for persons, companies or both.
+
+    Parameters
+    ----------
+    n : int
+        The number of names intended to get. Maximum supported 20.000.
+    name_type : str, optional
+        The type of name wanted, can be 'company', 'person' or 'any', by default 'any'.
+
+    Returns
+    -------
+    list
+        A list of len(n) with the names.
+
+    Raises
+    ------
+    KeyError
+        If type of name provided does not match any of the one supported.
+    
+    Examples
+    --------
+
+    """
+    if seed:
+        random.seed(seed)
     if n > 20000:
         warnings.warn('Number of samples too big, could cause the application to break. Returning 20.000 samples.')
         n = 20000
@@ -257,39 +380,87 @@ def random_name_generator(name_type:str, n:int):
         names = pd.concat([persons, companies])
         del persons, companies
 
-    result = names.sample(n).to_numpy()
+    result = names.sample(n).to_list()
     
     return result
 
 
-def mandato_generator():
-    if np.random.randint(0,3):
-        years = np.random.randint(1,11)
-        keywords = ['años', 'ejercicios'][np.random.randint(0,2)]
+def mandato_generator(seed:int=None) -> str:
+    """ Creates a string typically used for mandato lenght of companies.
+
+    Parameters
+    ----------
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    str
+        A string with a phrase typically used for mandato length.
+    """
+    if seed:
+        random.seed(seed)
+    if random.randint(0,2):
+        years = random.randint(1,10)
+        keywords = ['años', 'ejercicios'][random.randint(0,1)]
         years_words = num2words(years, lang='es')
-        random_year = [years, years_words, f'{years_words} ({years})', f'{years} ({years_words})'][np.random.randint(0,4)]
+        random_year = [years, years_words, f'{years_words} ({years})', f'{years} ({years_words})'][random.randint(0,3)]
         result = f'{random_year} {keywords}'
     else:
-        result = ['término de duración de la sociedad', 'plazo de duración de la sociedad', 'vencimiento de la sociedad'][np.random.randint(0,3)]
+        result = ['término de duración de la sociedad', 'plazo de duración de la sociedad', 'vencimiento de la sociedad'][random.randint(0,2)]
     
     return result
 
 
-def vigencia_generator():
-    years = np.random.randint(1,101)
+def vigencia_generator(seed:int=None) -> str:
+    """ Creates a string typically used for the duration of the company.
+
+    Parameters
+    ----------
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    str
+        A string with a phrase or number used to determine the duration of the company.
+    """
+    if seed:
+        random.seed(seed)
+    years = random.randint(1,100)
     years_words = num2words(years, lang='es')
-    salad = [years, years_words, f'{years_words} ({years})', f'{years} ({years_words})'][np.random.randint(0,4)]
+    result = [
+        years, 
+        years_words, 
+        f'{years_words} ({years})', 
+        f'{years} ({years_words})'
+    ]\
+    [random.randint(0,3)]
     
-    return salad
+    return result
 
 
-def tipicidad_generator():
+def tipicidad_generator(seed:int=None) -> str:
+    """ Creates a that determines the type of company.
+
+    Parameters
+    ----------
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    str
+        A string with a phrase containing the type of company.
+    """    
+    if seed:
+        random.seed(seed)
     company_type = ['sociedad de responsabilidad limitada', 
                     'sociedad anónima',
                     'sociedad por acciones simplificada',
                     'sociedad anónima unipersonal',
-                    'sociedad por acciones simplificada unipersonal'][np.random.randint(0, 5)]
-    style = ['lower', 'upper', 'title'][np.random.randint(0,3)]
+                    'sociedad por acciones simplificada unipersonal'][random.randint(0, 4)]
+    style = ['lower', 'upper', 'title'][random.randint(0,2)]
     
     if style == 'lower':
         result = company_type
@@ -301,10 +472,26 @@ def tipicidad_generator():
     return result
 
 
-def id_generator(cuit=False):
-    millions = np.random.randint(0,100)
-    thousands = np.random.randint(0,1000)
-    hundreds = np.random.randint(0,1000)
+def id_generator(cuit=False, seed:int=None) -> str:
+    """ Generates an argentine DNI or CUIT in string format.
+
+    Parameters
+    ----------
+    cuit : bool, optional
+        If True, will return a CUIT, otherwise a DNI, by default False.
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    str
+        A string containing the typical format of a DNI or a CUIT.
+    """
+    if seed:
+        random.seed(seed)
+    millions = random.randint(0,99)
+    thousands = random.randint(0,999)
+    hundreds = random.randint(0,999)
     
     
     if thousands < 10:
@@ -325,17 +512,40 @@ def id_generator(cuit=False):
 
     if cuit:
         millions = f'0{millions}' if millions < 10 else f'{millions}'
-        beginning = np.random.randint(20,36)
-        end = np.random.randint(1,10)
+        beginning = random.randint(20,35)
+        end = random.randint(1,9)
         result = f'{beginning}-{millions}{thousands}{hundreds}-{end}'
     
     return result
 
 
-def capital_generator(style:str='any'):
-    millions = np.random.randint(0,2)
-    thousands = [x for x in range(0,1000, 5)][np.random.randint(0,200)]
-    hundreds = ['000', '500'][np.random.randint(0,2)]
+def capital_generator(style:str='any', seed:int=None) -> str:
+    """ Creates a random string to define the funding of a company.
+
+    Parameters
+    ----------
+    style : str, optional
+        If set to 'written' will return the words of the amount, 
+        if set to 'number', will return just the amount,
+        if set to 'mixed', will return a mixture of the previous, by default 'any'.
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    str
+        A string containing a phrase, number ot both containing a money amount.
+
+    Raises
+    ------
+    KeyError
+        If the style specified doesn't exist.
+    """
+    if seed:
+        random.seed(seed)
+    millions = random.randint(0,1)
+    thousands = [x for x in range(0,1000, 5)][random.randint(0,199)]
+    hundreds = ['000', '500'][random.randint(0,1)]
     
     if millions:
         if len(str(thousands)) == 1:
@@ -345,13 +555,13 @@ def capital_generator(style:str='any'):
         else:
             thousands = str(thousands)
         
-        millions = np.random.randint(1,11)
+        millions = random.randint(1,10)
         result = f'{millions}.{thousands}.{hundreds}'
     else:
         result = f'{thousands}.{hundreds}'
     
     if style == 'any':
-        style = ['written', 'number', 'mixed'][np.random.randint(0,3)]
+        style = ['written', 'number', 'mixed'][random.randint(0,2)]
     
     if style == 'written':
         result = ''.join(result.split('.'))
@@ -359,7 +569,7 @@ def capital_generator(style:str='any'):
     elif style == 'mixed':
         number = ''.join(result.split('.'))
         words = num2words(number, lang='es')
-        result = [f'{words} ($ {result})', f'${result} ({words})'][np.random.randint(0,2)]
+        result = [f'{words} ($ {result})', f'${result} ({words})'][random.randint(0,1)]
     elif style == 'number':
         pass
     else:
@@ -367,39 +577,78 @@ def capital_generator(style:str='any'):
     return result
 
 
-def aporte_generator(share_type:str = 'any'):
-    thousands = [x for x in range(0,1000, 5)][np.random.randint(0,200)]
-    hundreds = ['000', '500'][np.random.randint(0,2)]
+def aporte_generator(share_type:str = 'any', seed:int=None) -> str:
+    """ Creates a random string containing the amount of shares
+        that a shareholder is giving to the company.
+
+    Parameters
+    ----------
+    share_type : str, optional [currently not in use]
+        If you want cuotas or acciones in the output, by default 'any'.
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    str
+        A string containing the number that a shareholder is giving
+        to be part of the company.
+    """
+    if seed:
+        random.seed(seed)
+    thousands = [x for x in range(0,1000, 5)][random.randint(0,199)]
+    hundreds = ['000', '500'][random.randint(0,1)]
     
-    if np.random.randint(0,2):
+    if random.randint(0,1):
         result = f'{thousands}.{hundreds}'
     else:
         result = thousands
     return result
 
 
-def address_generator(n:int, legal:bool=False):
+def address_generator(n:int, legal:bool=False, seed:int=None) -> List[str]:
+    """
+        Creates a list of strings with fictionary addresses, 
+        using real streets, districts and provinces.
+
+    Parameters
+    ----------
+    n : int
+        The lenght of the output list.
+    legal : bool, optional
+        If set to True, will not return a street, number
+        or apartment number, by default False.
+    seed : int, optional
+        If specified, will return the same value always, by default None.
+
+    Returns
+    -------
+    List[str]
+        A list containing n amounts of strings that simulate real addresses.
+    """
     
+    if seed:
+        random.seed(seed)
     df_address = pd.read_csv('../../data/estatutos/external_sources/calles.csv', dtype=str)
     
     if legal:
         result = df_address['departamento'] + ', ' + df_address['provincia']
         result = result.sample(n)
     else:
-        altura = pd.Series([np.random.randint(0,5000) for i in range(n*5)])
+        altura = pd.Series([random.randint(0,5000) for i in range(n*5)])
         
-        numeros = [str(np.random.randint(0,10)) for i in range(10)]
+        numeros = [str(random.randint(0,9)) for i in range(10)]
         letras = 'A B C D E F G H I J'.split(' ')
         letras_numeros = letras + numeros
-        piso = pd.Series([np.random.randint(0,20) for i in range(n*3)])
-        tipo = pd.Series([['departamento', 'oficina'][np.random.randint(0,2)] for i in range(n*3)])
-        enumeracion = pd.Series([letras_numeros[np.random.randint(0,20)] for i in range(n*3)])
+        piso = pd.Series([random.randint(0,20) for i in range(n*3)])
+        tipo = pd.Series([['departamento', 'oficina'][random.randint(0,1)] for i in range(n*3)])
+        enumeracion = pd.Series([letras_numeros[random.randint(0,19)] for i in range(n*3)])
         full = 'piso ' + piso.astype(str) + ', ' + tipo + ' ' + enumeracion.astype(str)
         casa = 'casa ' + piso.astype(str) + ', ' + 'manzana' + ' ' + enumeracion.astype(str)
         full = pd.concat([full, full, casa]).sample(n*3)
         print(len(full))
         connector = [[', de la localidad de ', ', partido de ', ', ', ', departamento de ']\
-                     [np.random.randint(0,4)] for i in range(n*3)]
+                     [random.randint(0,3)] for i in range(n*3)]
         
         completo =  pd.Series(df_address['nombre'].sample(n*3).str.title().values) + \
                 ' ' +\
