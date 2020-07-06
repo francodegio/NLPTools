16146a5fa6ee1e56777a8fcef87dfd08d66ec8a9
Franco De Giovannini <fdegiovannini@moorea.io>
2020-07-06 18:05:51 -0300

    added a function that relies on the num2words library

--- src/nlptools/parsing.py
+++ src/nlptools/parsing.py
@@ -1,8 +1,9 @@
 from spa2num.converter import to_number
-from typing import Optional
+from typing import Union
+from num2words import num2words
 
 
-def words_to_numbers(text:str) -> Optional[str, int]:
+def words_to_numbers(text:str) -> Union[str, int]:
     """ Ported from the spa2num library. Uses spa2num.converter.to_number
     to perform the conversion.
 
@@ -25,3 +26,62 @@ def words_to_numbers(text:str) -> Optional[str, int]:
     """
     return to_number(text)
 
+
+def number_to_words(
+        number,
+        lang: str = 'es',
+        to: str = 'cardinal',
+        **kwargs
+    ) -> str:
+    """ Ported from the num2words library. Uses num2words.num2words
+        under the hood.
+        
+        Converts a number into a phrase containing the words that
+        spell the number provided. Supports different languages.
+        Can return an cardinal or ordinal representation of 
+        the number.
+
+    Parameters
+    ----------
+    number : {str, int, float}
+        A number with or without decimals.
+    lang : str, optional
+        Output language, by default 'es'.
+    to : str, optional
+        Type of grammatical expression. Can be either cardinal
+        or ordinal, by default 'cardinal'.
+    **kargs :
+        Uknown. Please refer to the num2words library to find out.
+
+    Returns
+    -------
+    str
+        A phrase with the number spelled into the desired language.
+
+    Examples
+    -------
+    >>> from nlptools.parsing import number_to_words
+    
+    >>> number_to_words(1541, lang='es')
+    'mil quinientos cuarenta y uno'
+    
+    >>> number_to_words(1541, lang='en')
+    'one thousand, five hundred and forty-one'
+    
+    >>> number_to_words(154.1, lang='es')
+    'ciento cincuenta y cuatro punto uno'
+    
+    >>> number_to_words(154.1, lang='en')
+    'one hundred and fifty-four point one'
+    
+    >>> number_to_words("2345", lang='es')
+    'dos mil trescientos cuarenta y cinco'
+    
+    >>> number_to_words(9, lang='es', to='cardinal')
+    'nueve'
+    
+    >>> number_to_words(9, lang='es', to='ordinal')
+    'noveno'
+    """
+    return num2words(number=number, lang=lang, to=to, **kwargs)
+    
